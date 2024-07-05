342f6f7e7d2cd09aa80b1abb868d822d3b931a7e
root <naeemnasser89@gmail.cm>
2024-07-04 18:32:19 -0700

    Add script to make request to catch_me endpoint

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # A script that makes a request to 0.0.0.0:5000/catch_me and causes the server to respond with "You got me!"
-
 curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
