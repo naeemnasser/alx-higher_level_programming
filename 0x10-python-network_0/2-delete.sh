863131063892fb4c6a3cbf2e236d0cda87c71532
root <naeemnasser89@gmail.cm>
2024-07-04 17:35:14 -0700

    2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# A script that sends a DELETE request to the URL passed as the first argument
-# and displays the body of the response.
-
+# A script that sends a DELETE request to the URL passed as the first ar.
 curl -sX "DELETE" "$1"
-
