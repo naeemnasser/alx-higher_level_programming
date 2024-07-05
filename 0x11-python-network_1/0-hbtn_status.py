7433b71cba9bc3db76564e7a846d1f4a2c5f8537
root <naeemnasser89@gmail.cm>
2024-07-05 08:37:05 -0700

    Add script to fetch status and README files

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+and displays the body of the response.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
