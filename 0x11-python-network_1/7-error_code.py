edd7bb5ebaeb0282f72f740a4d3c688562ce1142
root <naeemnasser89@gmail.cm>
2024-07-06 16:05:27 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,10 +8,22 @@ Error code: followed by the value of the HTTP status code
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+
+def fetch_url(url):
+    """
+    Fetches the content of a given URL and prints it.
+
+    Args:
+        url (str): The URL to fetch content from.
+    """
     response = requests.get(url)
     print(response.text)
 
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_url(url)
+
